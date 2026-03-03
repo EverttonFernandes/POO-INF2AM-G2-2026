6c028215c417af46d3dff7c53c73630d66eb1c49
EvertonOliveiraFernandes <everton@umov.me>
2026-03-03 12:03:17 -0300

    Resolucao do primeiro exercicio

--- src/Aula02/Dia0303ComecandoComJava/Exercicio1/Main.java
+++ src/Aula02/Dia0303ComecandoComJava/Exercicio1/Main.java
@@ -1,7 +1,31 @@
 package Aula02.Dia0303ComecandoComJava.Exercicio1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Usando a biblioteca Scanner, informem 3 numeros. E depois digam qual desses numeros é o maior?
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro numero: ");
+        int primeiroNumero = scanner.nextInt();
+
+        System.out.println("Informe o primeiro numero: ");
+        int segundoNumero = scanner.nextInt();
+
+        System.out.println("Informe o primeiro numero: ");
+        int terceiroNumero = scanner.nextInt();
+
+        if (primeiroNumero > segundoNumero && primeiroNumero > terceiroNumero) {
+            System.out.println("O primeiro numero é o maior " + primeiroNumero);
+        } else if (segundoNumero > primeiroNumero && segundoNumero > terceiroNumero) {
+            System.out.println("O segundo numero é o maior " + primeiroNumero);
+        } else if (terceiroNumero > segundoNumero && terceiroNumero > primeiroNumero) {
+            System.out.println("O terceiro numero é o maior " + primeiroNumero);
+        } else {
+            System.out.println("Os numeros são iguais!");
+        }
+
     }
 }
--- src/Aula02/Dia0303ComecandoComJava/Exercicio2/Main.java
+++ src/Aula02/Dia0303ComecandoComJava/Exercicio2/Main.java
@@ -1,4 +1,9 @@
 package Aula02.Dia0303ComecandoComJava.Exercicio2;
 
 public class Main {
+    public static void main(String[] args) {
+        // Usando o scanner, informe 1 numero e depois usando um for some 1 até o numero informado.
+        // Exemplo: se o usuario digitar 3 o resultado deve ser 6 (1 + 2 + 3 = 6)
+        // use uma variavel total para acumular na soma usando o operador +=
+    }
 }
